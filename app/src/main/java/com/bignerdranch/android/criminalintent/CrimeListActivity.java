841d5b8a28001e3d57ddf0459ae78708fdf1d57a
Thomas Roberts <t.92.roberts@gmail.com>
2018-03-06 23:30:49 +0100

    Chapter 8
    Generic Fragment injection, and RecyclerView

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by Tom on 06/03/2018.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
