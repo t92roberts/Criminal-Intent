cc11cbd10e7d0269d0dfd3619a0bfa7111ade77c
Thomas Roberts <t.92.roberts@gmail.com>
2018-03-14 18:40:45 +0100

    Chapter 14
    SQLite database

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.criminalintent.database;
+
+/**
+ * Created by Tom on 14/03/2018.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
